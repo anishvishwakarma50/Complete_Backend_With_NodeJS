81b6f82972ff43182d1a5276eba063268d767774
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-03-30 17:46:16 +0530

    CRUD operation of Note has been done

--- Task/server.js
+++ Task/server.js
@@ -1,13 +1,6 @@
 // here we will import the instence of app to make it run
 const app = require("./src/app")
 
-app.use("express.json")
-
-app.post("/notes", (req, res) => {
-    const data = req.body;
-    
-})
-
 // Here we gooooo...
 app.listen(3000, () => {
     console.log("the server started running on port 3000")
--- Task/src/app.js
+++ Task/src/app.js
@@ -4,6 +4,49 @@ const express = require('express')
 // create instance of server using the name app
 const app = express()
 
+// This allows app to understand json
+app.use(express.json())
+
+const notes = [];
+
+// title, description
+// POST /notes
+app.post("/notes", (req, res) => {
+    notes.push(req.body);
+    res.status(201).json({
+        message: "notes created successfully"
+    })
+})
+
+// GET /notes
+app.get("/notes",(req, res) => {
+    res.status(200).json({
+        message: "notes fetched successfully",
+        notes: notes
+    });
+})
+
+// PATCH /notes/:index
+app.patch("/notes/:index", (req, res) => {
+    const index = req.params.index;
+    const description = req.body.description;
+
+    notes[index].description = description;
+    res.status(200).json({
+        message: "Notes Updated Successfully"
+    })
+})
+
+// DELETE /notes/$id
+app.delete("/notes/:index", (req, res) => {
+    const index = req.params.index;
+    delete notes[index]
+
+    res.status(200).json({
+        message : "Note Deleted Successfully"
+    })
+})
+
 
 // lets export this server so that it can be get utilized
 module.exports = app;
