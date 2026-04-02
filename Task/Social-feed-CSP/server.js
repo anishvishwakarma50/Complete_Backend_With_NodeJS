4febcf94c2b566e2db82b939f8d5ca09d19d5470
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-04-02 23:02:37 +0530

    New Task Social feed with Cloud Service Provider is initiated

---
+++ Task/Social-feed-CSP/server.js
@@ -0,0 +1,14 @@
+// This is an Simple application which consist of two API's
+// 1. create-post API for creating post with Image + Caption
+// 2. feed API for returning Feed with object
+// This task is little bit important because here we are going to use cloud service provider for image upload (imagekit)
+
+// let's start
+const app = require("./src/app")
+const connectDB = require("./src/db/db")
+
+connectDB()
+
+app.listen(3000, () => {
+    console.log("Server is Started Successfully...")
+})
