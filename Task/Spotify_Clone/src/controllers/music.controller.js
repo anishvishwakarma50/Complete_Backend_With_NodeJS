2183df25c116f6e6ebe767539da9d2cca320888c
anishvishwakarma50 <vishwakarmaanish50@gmail.com>
2026-05-14 23:01:35 +0530

    Music Controller Update

--- Task/Spotify_Clone/src/controllers/music.controller.js
+++ Task/Spotify_Clone/src/controllers/music.controller.js
@@ -1,5 +1,26 @@
-async function createMusic() {
-    
+const musicModel = require("../models/music.model")
+const jwt = require("jsonwebtoken")
+
+async function createMusic(req, res) {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({ message : "Unauthorized" })
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(decode.role !== "artist") {
+            return res.status(401).json({ message : "Access Denied!" })
+        }
+
+        const { title } = req.body;
+        const file = req.file;
+        
+    } catch(err) {
+        return res.status(401).json({ message : "Unauthorized" })
+    }
 }
 
 module.exports = { createMusic }
